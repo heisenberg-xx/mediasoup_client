230d02f302c987f182525b1c75f0a3539f12db0c
raju0005 <anderajesh15@gmail.com>
2025-10-16 23:31:52 +0530

    Changing Ip to try to stable ice connection

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://34.93.211.93:3000", {
+export const socket = io("https://34.93.216.76:3000", {
   transports: ["websocket"],
 });
